d7a8e0b436b29205e7acc6f2e0ba0ab8506ab4b5
Fayecodeur <fayecodeur99@gmail.com>
2025-05-10 20:16:55 +0000

    Ajout du composant CurrencyConverter

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import CurrencyConverter from "./components/CurrencyConverter";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-[#fff9db] via-[#fce38a] to-[#f9d976]">
+      <CurrencyConverter />
+    </main>
+  );
 }
